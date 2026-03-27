5517f684cb8b9a34bb4647fb66ddc709662f14dc
Ilham kurniawan <ilhamkurniawa1212@gmail.com>
2026-03-27 09:16:56 +0700

    testing

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
-    echo "Hello World!";
-    echo "TESTING";
+
 ?>   
+   <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   </head>
+   <body>
+        <div>
+            <h1>Welcome to my website</h1>
+             <p>This is a simple PHP website.</p>
+             <form action="login.php" method="post">
+              <label for="username">Username:</label>
+              <input type="text" id="username" name="username">
+              <br><br>
+              <label for="password">Password:</label>
+              <input type="password" id="password" name="password">
+              <input type="submit" value="Login">
+             </form>
+        </div>
+   </body>
+   </html>
