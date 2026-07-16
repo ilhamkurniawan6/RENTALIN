becdcb1d0b1b36979506b201cce64f44b766bfda
Muhammad Risyaq Fardan <risyaqgantenk@gmail.com>
2026-07-16 18:00:04 +0700

    delate sampah

--- _tmp_mid20.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/src/services/koneksi.php';
-$result = $conn->query('SELECT id, name FROM users WHERE id BETWEEN 1 AND 20 ORDER BY id ASC');
-while ($row = $result->fetch_assoc()) {
-    echo $row['id'] . ' | ' . $row['name'] . PHP_EOL;
-}
